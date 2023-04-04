aeb63a725abc502ab57e5f8ee18e7015c7f675c9
ahmed09ashraf <ahmedashraf097@gmail.com>
2023-04-04 15:23:50 +0200

    first push

---
+++ database/migrations/2023_04_04_131550_create_employees_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lasttname');
+            $table->string('email')->unique();
+            $table->biginteger('phone');
+            $table->foreignId('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
